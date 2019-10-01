278f6a9570bad800070e1509768f5ba7cddd32d5
jamesyothers <jamesjsdev@gmail.com>
2019-10-01 16:25:25 -0500

    bugfix: fix app build

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -185,10 +185,10 @@ public class EarthquakeActivity extends AppCompatActivity
         //mAdapter.clear();
         
         // If there is a valid list of {@link Earthquake}s, then add them to the adapter's
-        // data set. This will trigger the ListView to update.
+        // data set. This will trigger themAdapter.addAll(earthquakes) ListView to update.
         if (earthquakes != null && !earthquakes.isEmpty()) {
-            //mAdapter.addAll(earthquakes);
-            updateUi(earthquakes);
+            mAdapter.addAll(earthquakes);
+//            updateUi(earthquakes);
         }
     }
 
